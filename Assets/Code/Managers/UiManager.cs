094649a1c72bd82b30e46ec0bef2bdc2b4d7bcc7
Luka <lukaverzijl@hotmail.com>
2026-03-23 13:47:52 +0100

    Ui Basics

---
+++ Assets/Code/Managers/UiManager.cs
@@ -0,0 +1,15 @@
+using Code.Ui;
+using Code.Utils;
+
+namespace Code.Managers
+{
+    public class UiManager :Singleton<UiManager>
+    {
+        #region Modal
+
+        public LoginModal _loginModal;
+
+        #endregion
+
+    }
+}
---
+++ Assets/Code/Ui/LoginModal.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Code.Ui
+{
+    public class LoginModal : MonoBehaviour
+    {
+        
+    }
+}
