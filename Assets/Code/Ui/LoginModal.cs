ea30a2dbe74c479fd98a7d9534382ece6f086ef9
Luka <lukaverzijl@hotmail.com>
2026-03-31 11:45:56 +0200

    login ui linkup

--- Assets/Code/Ui/LoginModal.cs
+++ Assets/Code/Ui/LoginModal.cs
@@ -1,9 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 namespace Code.Ui
 {
     public class LoginModal : MonoBehaviour
     {
+        [SerializeField] private TMP_InputField emailInput;
+        [SerializeField] private TMP_InputField passwordInput;
+
+
+        public async void Login()
+        {
+            ApiManager.Instance.user.Email = emailInput.text;
+            ApiManager.Instance.user.Password = passwordInput.text;
+            await ApiManager.Instance.Login();
+        }
+
+        public async void Register()
+        {
+            ApiManager.Instance.user.Email = emailInput.text;
+            ApiManager.Instance.user.Password = passwordInput.text;
+            await ApiManager.Instance.Register();
+        }
+
         
     }
 }
