3d74c6f358bbe5f6af2d7595754cce9038d24f92
Luka <lukaverzijl@hotmail.com>
2026-04-02 09:58:49 +0200

    validation + redirect

--- Assets/Code/Ui/LoginModal.cs
+++ Assets/Code/Ui/LoginModal.cs
@@ -1,3 +1,4 @@
+using Code.Managers;
 using TMPro;
 using UnityEngine;
 
@@ -7,20 +8,48 @@ namespace Code.Ui
     {
         [SerializeField] private TMP_InputField emailInput;
         [SerializeField] private TMP_InputField passwordInput;
+        [SerializeField] private TMP_Text validationText;
+        private bool isLoggingIn = false;
 
 
         public async void Login()
         {
+            if (isLoggingIn) return;
+            isLoggingIn = true;
             ApiManager.Instance.user.Email = emailInput.text;
             ApiManager.Instance.user.Password = passwordInput.text;
-            await ApiManager.Instance.Login();
+            if (await ApiManager.Instance.Login())
+            {
+                Debug.Log("Login succes!");
+                UiManager.Instance.HideAllUI();
+                UiManager.Instance.ShowParentControlModal();
+                isLoggingIn = false;
+            }
+            else
+            {
+                Debug.Log("Login failed!");
+                isLoggingIn = false;
+                validationText.text = "Login failed! Please check your credentials and try again.";
+
+            }
         }
 
         public async void Register()
         {
+            if (isLoggingIn) return;
+            isLoggingIn = true;
             ApiManager.Instance.user.Email = emailInput.text;
             ApiManager.Instance.user.Password = passwordInput.text;
-            await ApiManager.Instance.Register();
+            if (await ApiManager.Instance.Register())
+            { 
+                isLoggingIn = false;
+                UiManager.Instance.ShowParentControlModal();
+            }
+            else
+            {
+                isLoggingIn = false;
+                validationText.text = "Registration failed! Please check your credentials and try again.";
+            }
         }
 
         
